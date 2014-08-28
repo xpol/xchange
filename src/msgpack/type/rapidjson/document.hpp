20919b65fc598b62c31e9a06e03dcbbe04de58f3
xpol <xpolife@gmail.com>
2014-08-28 13:42:36 +0800

    Move into src dir.

--- src/msgpack/type/rapidjson/document.hpp
+++ src/msgpack/type/rapidjson/document.hpp
@@ -1,5 +1,5 @@
-#ifndef MSGPACK_TYPE_RAPIDJSON_GENERICVALUE_HPP__
-#define MSGPACK_TYPE_RAPIDJSON_GENERICVALUE_HPP__
+#ifndef MSGPACK_TYPE_RAPIDJSON_DOCUMENT_HPP__
+#define MSGPACK_TYPE_RAPIDJSON_DOCUMENT_HPP__
 
 #include "msgpack/object.hpp"
 
@@ -18,29 +18,31 @@ namespace msgpack {
 		case msgpack::type::NEGATIVE_INTEGER: v.SetInt64(o.via.i64); break;
 		case msgpack::type::DOUBLE: v.SetDouble(o.via.dec); break;
 		case msgpack::type::RAW: v.SetString(o.via.raw.ptr, o.via.raw.size); break;
-		case msgpack::type::ARRAY:
+		case msgpack::type::ARRAY:{
 			v.SetArray();
 			msgpack::object* ptr = o.via.array.ptr;
 			msgpack::object* END = ptr + o.via.array.size;
 			for (; ptr < END; ++ptr)
 			{
-				Value element;
+				rapidjson::GenericValue<Encoding, Allocator> element;
 				ptr->convert(&element);
-				v.PushBack(element);
+				v.PushBack(element, Allocator());
 			}
+		}
 			break;
-		case msgpack::type::MAP:
+		case msgpack::type::MAP: {
 			v.SetObject();
 			msgpack::object_kv* ptr = o.via.map.ptr;
 			msgpack::object_kv* END = ptr + o.via.map.size;
 			for (; ptr < END; ++ptr)
 			{
-				Value key, val;
+				rapidjson::GenericValue<Encoding, Allocator> key, val;
 				ptr->key.convert(&key);
 				ptr->val.convert(&val);
 
-				v.AddMember(key, val);
+				v.AddMember(key, val, Allocator());
 			}
+		}
 			break;
 		case msgpack::type::NIL:
 		default:
@@ -78,7 +80,7 @@ namespace msgpack {
 		if (v.IsArray())
 		{
 			o.pack_array(v.Size());
-			rapidjson::Value::ConstValueIterator i = v.Begin(), END = v.End();
+			rapidjson::GenericValue<Encoding, Allocator>::ConstValueIterator i = v.Begin(), END = v.End();
 			for (;i < END; ++i)
 				o.pack(*i);
 			return o;
@@ -86,14 +88,11 @@ namespace msgpack {
 
 		if (v.IsObject())
 		{
-			rapidjson::Value::ConstMemberIterator i = v.MemberBegin(), END = v.MemberEnd();
-				size_t sz = 0;
-			for (; i < END; ++i; ++sz)
-				continue;
-
+			rapidjson::GenericValue<Encoding, Allocator>::ConstMemberIterator i = v.MemberBegin(), END = v.MemberEnd();
+			size_t sz = END - i;
 			o.pack_map(sz);
 			i = v.MemberBegin();
-			for (; i < END; ++i; ++sz)
+			for (; i < END; ++i)
 			{
 				o.pack(i->name);
 				o.pack(i->value);
@@ -179,7 +178,7 @@ namespace msgpack {
 				object_kv* const pend = p + sz;
 				o.via.map.ptr = p;
 				o.via.map.size = sz;
-				rapidjson::Value::ConstMemberIterator it(v.MemberBegin());
+				rapidjson::GenericValue<Encoding, Allocator>::ConstMemberIterator it(v.MemberBegin());
 				do {
 					p->key = object(it->name, o.zone);
 					p->val = object(it->value, o.zone);
@@ -192,5 +191,5 @@ namespace msgpack {
 }
 
 
-#endif /* msgpack/type/rapidjson/value.hpp */
+#endif /* msgpack/type/rapidjson/document.hpp */
 
